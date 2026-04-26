aae9734cfd20be91743c601f713745051e3b0193
hekciu <jakublasecki321@gmail.com>
2026-04-26 22:03:50 +0200

    utilize getopt_long for arguments

--- main.c
+++ main.c
@@ -3,34 +3,59 @@
 #include <stdbool.h>
 #include <string.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <getopt.h>
 
 #include "error_codes.h"
 #include "common.h"
 #include "copy.h"
 
+static struct option long_opt[] =
+{
+  {"help", no_argument, NULL, 'h'},
+  {"input", required_argument, NULL, 'i'},
+  {"output", required_argument, NULL, 'o'},
+  {"raw", required_argument, NULL, 'r'},
+  {NULL, 0, NULL, 0}
+};
 
-
-bool compare_arg(int argc, char** argv, uint32_t index, const char* value) {
-    if (argc <= index) return false;
-
-    return strcmp(argv[index], value) == 0;
-}
-
+static const char*  short_opt = "hior";
 
 int main(int argc, char** argv) {
-    if (compare_arg(argc, argv, 1, "--help")) {
-        fcp_print_help_message();
-
-        return 0;
-    }
-
-    if (argc < 3) fcp_exit(FCP_BAD_ARGUMENTS);
-
     fcp_copy_config_t config = {0};
     fcp_copy_output_t output = {0};
 
-    config.src = argv[1];
-    config.dest = argv[2];
+    char c;
+
+    while((c = getopt_long(argc, argv, short_opt, long_opt, NULL)) != -1) {
+        switch(c)
+        {
+            case -1:       /* no more arguments */
+            case 0:        /* long options toggles */
+            break;
+
+            case 'h':
+            fcp_print_help_message();
+            return 0;
+            break;
+
+            case 'i':
+            config.src = argv[optind];
+            break;
+
+            case 'o':
+            config.dest = argv[optind];
+            break;
+
+            case 'r':
+            break;
+
+            default:
+            fcp_exit(FCP_BAD_ARGUMENTS);
+        };
+    }
+
+    if (config.src == NULL || config.dest == NULL) fcp_exit(FCP_BAD_ARGUMENTS);
 
     HANDLE_ERROR(fcp_copy(&config, &output));
 
