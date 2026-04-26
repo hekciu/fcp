b47eadffe9dea98115f75c9d88019907d0c222a9
hekciu <jakublasecki321@gmail.com>
2026-04-26 15:01:20 +0200

    fixed HANDLE_ERROR macro

--- inc/common.h
+++ inc/common.h
@@ -10,8 +10,20 @@
 void fcp_exit(FCP_ERROR err);
 void fcp_print_help_message();
 
-#define SYSCALL_ERR_HANDLE(name, val) if(val < 0) {fprintf(stderr, "%s failed, details: \n%s\n", name, strerrordesc_np(errno));return FCP_SYSCALL_FAILED;}
+#define SYSCALL_ERR_HANDLE(name, call) do { if((call) < 0) {fprintf(stderr, "%s failed, details: \n%s\n", name, strerrordesc_np(errno));return FCP_SYSCALL_FAILED;} } while(0)
 
-#define HANDLE_ERROR(fcp_err) {if ((fcp_err) != FCP_OK) { fcp_exit(fcp_err); }}
+#define HANDLE_ERROR(call) \
+{ \
+    do \
+    { \
+        FCP_ERROR res = (call); \
+        if (res != FCP_OK) \
+        { \
+            fcp_exit(res);\
+        } \
+    } \
+    while(0); \
+}
+
 
 #endif
